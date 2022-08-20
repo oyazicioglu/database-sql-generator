22a3f0d4eb1d6b334877f521cfe2f635b370fdcd
Ömer YAZICIOĞLU <oyazicioglu@hotmail.com>
2022-08-20 21:17:49 +0300

    Correction on PostgresDatabaseBuilder

--- src/builders/Postgres/PostgresDatabaseBuilder.ts
+++ src/builders/Postgres/PostgresDatabaseBuilder.ts
@@ -1,18 +1,19 @@
-import { Pool } from 'pg';
+import { Pool, QueryResult } from 'pg';
 import { AbstractDatabase } from '../AbstractDatabase';
 import { IDatabaseBuilder } from '../IDatabaseBuilder';
 
 class PostgresDatabaseBuilder extends AbstractDatabase implements IDatabaseBuilder {
     constructor(name: string, createIfNotExists: boolean = true) {
         super(name, createIfNotExists);
+        this.name = name;
+        this.createIfNotExists = createIfNotExists;
     }
 
-    Build(pool: Pool): Promise<Error | undefined> {
-        const sqlArray = [`CREATE DATABASE ${this.name}`, this.createIfNotExists && 'IF NOT EXISTS'];
+    Build(pool: Pool): Promise<Error | QueryResult | undefined> {
         return new Promise((resolve, reject) => {
-            pool.query(sqlArray.join(' '), (err: Error) => {
-                if (err) return resolve(err);
-                return resolve(undefined);
+            pool.query(`CREATE DATABASE ${this.name}`, (err: Error, result: QueryResult) => {
+                if (err) return reject(err);
+                return resolve(result);
             });
         });
     }
